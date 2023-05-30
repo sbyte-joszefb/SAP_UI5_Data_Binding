74040172d4dc81dea3f9e8cf491ce1537937ec7c
Joszef <joszef.sinosbyte@gmail.com>
2023-05-30 14:34:29 -0300

    Passo 15 - Associação Associação de agregação por Factory Function

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,9 +4,10 @@ sap.ui.define(
         "sap/m/library",
         "sap/ui/core/Locale",
         "sap/ui/core/LocaleData",
-        "sap/ui/model/type/Currency" 
+        "sap/ui/model/type/Currency",
+        "sap/m/ObjectAttribute"
     ],
-    function (Controller, mobileLibrary, Locale, LocaleData, Currency) {
+    function (Controller, mobileLibrary, Locale, LocaleData, Currency, ObjectAttribute) {
         "use strict"
 
         return Controller.extend("sap.ui.demo.db.controller.App",
@@ -32,6 +33,29 @@ sap.ui.define(
                     var sPath = oContext.getPath();
                     var oProductDetailPanel = this.byId("productDetailsPanel");
                     oProductDetailPanel.bindElement({ path: sPath, model: "products"});
+                },
+                productListFactory: function(sId, oContext) {
+                    var oUIControl;
+        
+                    // Decide com base nos dados que dependem para clonar
+                    if (oContext.getProperty("UnitsInStock") === 0 && oContext.getProperty("Discontinued")) {
+                        // O item foi descontinuado, então use um StandardListItem
+                        oUIControl = this.byId("productSimple").clone(sId);
+                    } else {
+                        // O item está disponível, então vamos criar um ObjectListItem
+                        oUIControl = this.byId("productExtended").clone(sId);
+        
+                        // o item está temporariamente fora de estoque, então adicionaremos um status
+                        if (oContext.getProperty("UnitsInStock") < 1) {
+                            oUIControl.addAttribute(new ObjectAttribute({
+                                text : {
+                                    path: "i18n>outOfStock"
+                                }
+                            }))
+                        }
+                    }
+        
+                    return oUIControl;
                 }
             }
         )
